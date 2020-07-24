a3955c0992bd70c3f61d1a6b7c5fb3fe61eeba91
Evan Pavone <evanrpavone@gmail.com>
2020-07-24 14:32:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function currentLine(katzDeli) {
     i++;
   }
   if (katzDeli.length === 0) {
-    return "The line is currentl empty.";
+    return "The line is currently empty.";
   } else
   return(`The line is currently: ` + line);
 }
