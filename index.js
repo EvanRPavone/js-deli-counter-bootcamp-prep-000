3f2bd79a73eef86c1fc691d0ab3a589c64ae1507
Evan Pavone <evanrpavone@gmail.com>
2020-07-24 14:31:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,17 @@ function nowServing(katzDeli) {
   else
   return (`Currently serving ${katzDeli.shift()}.`);
 }
+
+var line = [];
+
+function currentLine(katzDeli) {
+  let i=0;
+  while (i < katzDeli.length) {
+    line.push(` `+[i+1]+`. ` + katzDeli[i])
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currentl empty.";
+  } else
+  return(`The line is currently: ` + line);
+}
